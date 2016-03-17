5b38fee0865755c62ab81c0b24929a13c1614cb1
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-17 11:39:23 -0600

    home routes

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
 router.get('/', function(req, res, next)
 {
-  var request = require('request');
-  request('http://www.google.com', function (error, response, body)
-  {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
-  });
+  res.render('products.html');
 });
 
 router.get('/Product', function(req, res, next)
