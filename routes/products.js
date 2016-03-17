7dacb619771ae1e0edf3fe13ef11fcb87087ae49
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-17 14:04:07 -0600

    adding

--- routes/products.js
+++ routes/products.js
@@ -1,29 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 router.get('/', function(req, res, next)
 {
   res.render('products.html');
 });
 
-// router.get('/Product', function(req, res, next)
-// {
-//   var request = require('request');
-//   request('http://www.google.com', function (error, response, body)
-//   {
-//     if (!error && response.statusCode == 200) {
-//       console.log(body) // Show the HTML for the Google homepage.
-//     }
-//   });
-// });
-
-router.get('/Product/Review', function(req, res, next)
+router.get('/Review', function(req, res, next)
 {
-  var request = require('request');
   request('http://www.google.com', function (error, response, body)
   {
     if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage.
+    }
+  });
+});
+router.get('/Google', function(req, res, next)
+{
+  // var request = require('request');
+  request('http://www.google.com', function (error, response, body)
+  {
+    if (!error && response.statusCode == 200)
+    {
+      console.log(body) // Show the HTML for the Google homepage.
+      res.send(body);
+    }
+  })
+});
+
+router.post('/Product/Review', function(req, res, next)
+{
+  request.post('http://www.google.com', function (error, response, body)
+  {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Show the HTML for the Google homepage.
+    }
+    else
+    {
+
     }
   });
 });
