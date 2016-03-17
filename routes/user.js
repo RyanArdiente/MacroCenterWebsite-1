7fe10ff65bd50a4624f143686238fb98e70c92bd
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-17 16:55:54 -0600

    almost got it

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET users listing. */
 // router.get('/', function(req, res, next)
@@ -28,12 +29,21 @@ var router = express.Router();
 
 router.get('/', function(req, res, next)
 {
+  console.log("here");
   request('http://localhost:8080/MacroCenter/rest/user', function (error, response, body)
   {
-    console.log(body) // Show the HTML for the Google homepage.
-    res.render('user', body);
     if (!error && response.statusCode == 200)
     {
+      // console.log(body) // Show the HTML for the Google homepage.
+      // res.render('user', body);
+      var users = JSON.parse(body);
+      res.render('product', {page : {
+        header : "user",
+        title : "test",
+        user : [users]
+      }
+      // res.send(body);
+    });
     }
   });
 });
