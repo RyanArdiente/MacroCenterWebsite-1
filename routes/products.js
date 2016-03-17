8f2b4c0719428dabd33ecdf0752f9fdf915228b7
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-17 13:24:38 -0600

    fixed

--- routes/products.js
+++ routes/products.js
@@ -4,18 +4,19 @@ var router = express.Router();
 router.get('/', function(req, res, next)
 {
   res.render('products.html');
+
 });
 
-router.get('/Product', function(req, res, next)
-{
-  var request = require('request');
-  request('http://www.google.com', function (error, response, body)
-  {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
-  });
-});
+// router.get('/Product', function(req, res, next)
+// {
+//   var request = require('request');
+//   request('http://www.google.com', function (error, response, body)
+//   {
+//     if (!error && response.statusCode == 200) {
+//       console.log(body) // Show the HTML for the Google homepage.
+//     }
+//   });
+// });
 
 router.get('/Product/Review', function(req, res, next)
 {
--- routes/shoppingcart.js
+++ routes/shoppingcart.js
@@ -6,16 +6,16 @@ router.get('/', function(req, res, next)
   res.render('shoppingCart.html');
 });
 
-router.get('/ShoppingCart', function(req, res, next)
-{
-  var request = require('request');
-  request('http://www.google.com', function (error, response, body)
-  {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Show the HTML for the Google homepage.
-    }
-  });
-});
+// router.get('/ShoppingCart', function(req, res, next)
+// {
+//   var request = require('request');
+//   request('http://www.google.com', function (error, response, body)
+//   {
+//     if (!error && response.statusCode == 200) {
+//       console.log(body) // Show the HTML for the Google homepage.
+//     }
+//   });
+// });
 
 router.get('/ShoppingCart/Checkout', function(req, res, next)
 {
