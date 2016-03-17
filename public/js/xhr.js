3be706fd86c83c384943595afe5e0d7b5b650fc3
Ryan Ardiente <Nayrdnt@Ryans-MacBook-Pro.local>
2016-03-17 12:00:21 -0600

    created xhr

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -4,25 +4,24 @@ xhr.open(method, url);
 xhr.onreadystatechange = function(){
   if(xhr.readyState == 4 && xhr.status < 400)
   {
-        if(method === "GET"){
-        callback(xhr.responseText);
-      }
-      else{
-        xhrTool("GET", "rest/getAllScores",updateTable);
-      }
+      callback(xhr.responseText);
+
   }
 };
-  if(object !== null)
-  {
-    xhr.setRequestHeader("Content-Type","application/json");
-    xhr.send(JSON.stringify(object));
-  }
-  else{
-    xhr.send(null);
-  }
 
+  xhr.send(null);
 };
 
 var xhrverb = function(method , url, callback, object){
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 && xhr.status < 400)
+    {
+        callback(xhr.responseText);
 
-}
+    }
+  };
+    xhr.setRequestHeader("Content-Type","application/json");
+    xhr.send(JSON.stringify(object));
+};
