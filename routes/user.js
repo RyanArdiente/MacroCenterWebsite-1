a26249518b4146ecf52bb3abb4e8eef754d8a3ba
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-18 10:05:28 -0600

    login/create user routes

--- routes/user.js
+++ routes/user.js
@@ -2,35 +2,55 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-/* GET users listing. */
-// router.get('/', function(req, res, next)
-// {
-//   res.render('user.html');
-//   res.send('respond with a resource');
-// });
-
-// router.get('/', function(req, res, next)
-// {
-//   request('http://localhost:8080/MacroCenter/rest/user', function (error, res, body)
-//   {
-//     if (!error && response.statusCode == 200)
-//     {
-//       console.log(body) // Show the HTML for the Google homepage.
-//       res.render('user', {page : {
-//         header : "header",
-//         title : "users",
-//         user : {}
-//       }
-//       // res.send(body);
-//     }
-//   });
-//   }
-// });
-
 router.get('/', function(req, res, next)
 {
-  console.log("here");
+  console.log("in user/");
   request('http://localhost:8080/MacroCenter/rest/user', function (error, response, body)
+  {
+    if (!error && response.statusCode == 200)
+    {
+      // console.log(body) // Show the HTML for the Google homepage.
+      // res.render('user', body);
+      var users = JSON.parse(body);
+      console.log(users);
+      res.render('user', {page : {
+        header : "user",
+        title : "test",
+        user : users
+      }
+      // res.send(body);
+    });
+    }
+  });
+});
+
+router.post('/createUser', function(req, res, next)
+{
+  console.log("in user/createUser");
+  var obj = req.body;
+  request('http://localhost:8080/MacroCenter/rest/createUser', body, function (error, response, body)
+  {
+    if (!error && response.statusCode == 200)
+    {
+      // console.log(body) // Show the HTML for the Google homepage.
+      // res.render('user', body);
+      var users = JSON.parse(body);
+      console.log(users);
+      res.render('user', {page : {
+        header : "user",
+        title : "test",
+        user : users
+      }
+      // res.send(body);
+    });
+    }
+  });
+});
+
+router.get('/login', function(req, res, next)
+{
+  console.log("in user/login");
+  request('http://localhost:8080/MacroCenter/rest/login', function (error, response, body)
   {
     if (!error && response.statusCode == 200)
     {
