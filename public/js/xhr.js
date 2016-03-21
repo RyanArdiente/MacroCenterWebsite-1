242e1bfef3f3c52a62b4741624c895e9615179fe
travisldunn <travisldunn@gmail.com>
2016-03-21 17:41:08 -0600

    js problem with anup

--- public/js/xhr.js
+++ public/js/xhr.js
@@ -1,15 +1,22 @@
-var xhrget = function(method,url, callback, object){
+var xhrget = function(method,url, callback, obj){
+  console.log("in get ajax function");
 var xhr = new XMLHttpRequest();
 xhr.open(method, url);
 xhr.onreadystatechange = function(){
   if(xhr.readyState == 4 && xhr.status < 400)
   {
-      callback(xhr.responseText);
+    var tempData = xhr.response;
+    console.log("response  from tomcat :" + tempData);
+      callback(tempData);
+      console.log("after callback, in xhr.js");
   }
 };
-
   xhr.send(null);
 };
+
+
+
+
 var xhrverb = function(method , url, callback, object)
 {
   console.log(method);
@@ -22,12 +29,13 @@ var xhrverb = function(method , url, callback, object)
   {
     if(xhr.readyState == 4 && xhr.status < 400)
     {
+      // console.log(xhr.response);
       if(callback)
       {
         callback(xhr.responseText);
       }
     }
-  }
+  };
   xhr.setRequestHeader("Content-Type","application/json");
   xhr.send(JSON.stringify(object));
 };
--- routes/products.js
+++ routes/products.js
@@ -3,62 +3,91 @@ var router = express.Router();
 var request = require('request');
 var app = express();
 
+console.log("product.js loaded");
+
 // app.get("/", function(req,res)
 // {
 // });
-router.get('/', function(req, res, next)
-{
+router.get('/', function(req, res, next) {
+  // console.log(req);
+  res.render('product', {
+    page: {
+      header: "testProductget",
+      // title : "testPro",
+      products: [{
+        name: "test1",
+        price: "$10"
+      }, {
+        name: "test2",
+        price: "$20"
+      }, {
+        name: "test3",
+        price: "30"
+      }],
 
-      res.render('product', {page : {
-        header : "testProduct",
-        title : "testPro",
-        products :  [
-          {
-            name : "test1",
-            price : "$10"
-          },
-          {
-            name : "test2",
-            price : "$20"
-          },
-          {
-            name : "test3",
-            price : "30"
-          }
-        ]
-
-      }
-      // res.send(body);
-    });
+      script: "<script  type='text/javascript' src='../js/xhr.js'></script>",
+      script2: "<script  type='text/javascript' src='../js/productClient.js'></script>"
+    }
+    // res.send(body);
+  });
   // }
   // });
 });
 //   res.render('about', {page : about.getAbout()});
 
-router.get('/Review', function(req, res, next)
-{
-  request('http://www.google.com', function (error, response, body)
-  {
-    if (!error && response.statusCode == 200)
-    {
-      console.log(body) // Show the HTML for the Google homepage.
+
+router.get('/test', function(req, res, next) {
+  console.log("inside testproduct route");
+
+   var testProduct = {};
+  request('http://localhost:8080/MacroCenter/rest/testProduct', function(error, r, body) {
+    // console.log(JSON.parse(res));
+    testProduct = (JSON.parse(body));
+
+console.log("INside requst to java Data is  " +testProduct + " and id is " + testProduct.id);
+// next();
+
+res.render('product', {
+  page: {
+    header: "testProductget",
+    // title : "testPro",
+    product: testProduct,
+
+
+    script: "<script  type='text/javascript' src='../js/xhr.js'></script>",
+    script2: "<script  type='text/javascript' src='../js/productClient.js'></script>"
+  }
+  // res.send(body);
+});
+  });
+console.log("Data is  " +testProduct + " and id is " + testProduct.id);
+
+
+
+});
+
+
+
+
+router.get('/Review', function(req, res, next) {
+  request('http://www.google.com', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      // Show the HTML for the Google homepage.
       res.send(body);
     }
   });
 });
 
-router.post('/:obj', function(req, res, next)
-{
+router.post('/:obj', function(req, res, next) {
   var obj = req.body;
   // var request = require('request');
-  request('http://www.google.com/'+obj, function (error, response, body)
-  {
-    if (!error && response.statusCode == 200)
-    {
+  request('http://www.google.com/' + obj, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
       console.log(body) // Show the HTML for the Google homepage.
       res.send(body);
     }
-  })
+  });
 });
 
 module.exports = router;
