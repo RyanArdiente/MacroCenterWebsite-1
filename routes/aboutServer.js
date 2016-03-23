04dee6ed54d0cf08063e9ad702a3703d65f61747
Christopher Bergstrom <christopherbergstrom@Christophers-MacBook-Pro.local>
2016-03-23 11:54:30 -0600

    contact us page

--- routes/aboutServer.js
+++ routes/aboutServer.js
@@ -31,4 +31,17 @@ router.get('/employee', function(req, res, next) {
     });
   });
 });
+
+router.get('/contact', function(req, res, next)
+{
+  console.log("inside contact route");
+    res.render('about', {
+      page: {
+        header: "Contact Us",
+        info: "Address: 7400 East Orchard Road, Suite 1450N, Greenwood Village, CO, 80111 <a href = \"https://www.google.com/maps/place/7400+E+Orchard+Rd,+Greenwood+Village,+CO+80111/@39.6088518,-104.9050174,17z/data=!3m1!4b1!4m2!3m1!1s0x876c86ed5f85ee1b:0x38551d73b5335c2b?hl=en-US%22%20target=%22_blank\"</a><br>Phone: 1-800-555-5555",
+        script: "<script  type='text/javascript' src='../js/aboutClient.js'></script>"
+      }
+    });
+});
+
 module.exports = router;
